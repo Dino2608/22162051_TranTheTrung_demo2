b8892b0b0017134bcdb05fd4a4cd7279ef532c49
Dino2608 <conbomap2004@gmail.com>
2024-11-25 14:21:31 +0700

    ?name&pass

---
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,26 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.entities.UserInfo;
+import com.example.demo.services.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/new")
+    public String addUser(@RequestBody UserInfo userInfo) {
+        return userService.addUser(userInfo);
+    }
+}
+
